b91e655042e3896d619e2d0d7617929b476f3b13
rodrigo.pereira <14523698>
2017-12-13 18:03:48 -0400

    pom.xml and PirApplication (main) fix

--- src/main/java/com/samsung/fas/pir/PirApplication.java
+++ src/main/java/com/samsung/fas/pir/PirApplication.java
@@ -3,11 +3,18 @@ package com.samsung.fas.pir;
 import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableJSONDoc
-public class PirApplication {
+public class PirApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(PirApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PirApplication.class);
 	}
 }
