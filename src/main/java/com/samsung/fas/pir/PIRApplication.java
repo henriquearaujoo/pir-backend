4deb1188dce4b741ac0c0669c8bc1b4f263acafe
Rodrigo Pereira <rodrigo.c.pereira@icloud.com>
2018-03-01 20:08:46 -0400

    [0.0.7] Updated Swagger Documentation Generator

--- src/main/java/com/samsung/fas/pir/PIRApplication.java
+++ src/main/java/com/samsung/fas/pir/PIRApplication.java
@@ -6,18 +6,19 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Controller;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.VendorExtension;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
+import java.util.List;
 
 @SpringBootApplication
 @EnableAsync
@@ -40,29 +41,39 @@ public class PIRApplication extends SpringBootServletInitializer {
 	@Bean
 	public Docket docket() {
 		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.withClassAnnotation(Controller.class))
-				.paths(PathSelectors.ant("/rest/**"))
-				.build()
-				.apiInfo(new ApiInfo(
-						"PIR REST Documentation",
-						"Documentation for PIR REST Services",
-						"0.0.7",
-						"Terms of Service: TODO",
-						new Contact("Instituto ITN", "http://www.institutoitn.com.br", "contato@institutoitn.com.br"),
-						"Apache License Version 2.0",
-						"https://www.apache.org/licenses/LICENSE-2.0",
-						Collections.singleton(new VendorExtension<String>() {
-							@Override
-							public String getName() {
-								return "Intituto de Tecnologia e Negócios do Norte";
-							}
+					   .select()
+					   .apis(RequestHandlerSelectors.withClassAnnotation(Controller.class))
+					   .paths(PathSelectors.ant("/rest/**"))
+					   .build()
+					   .pathMapping("/")
+					   .securitySchemes(Collections.singletonList(new ApiKey("APIKEY", HttpHeaders.AUTHORIZATION, "HEADER")))
+					   .securityContexts(Collections.singletonList(SecurityContext.builder().securityReferences(authorization()).forPaths(PathSelectors.ant("/rest/**")).build()))
+					   .apiInfo(new ApiInfo(
+							"PIR REST Documentation",
+							"Documentation for PIR REST Services",
+							"0.0.7",
+							"Terms of Service: TODO",
+							new Contact("Instituto ITN", "http://www.institutoitn.com.br", "contato@institutoitn.com.br"),
+							"Apache License Version 2.0",
+							"https://www.apache.org/licenses/LICENSE-2.0",
+							Collections.singleton(new VendorExtension<String>() {
+								@Override
+								public String getName() {
+									return "Intituto de Tecnologia e Negócios do Norte";
+								}
 
-							@Override
-							public String getValue() {
-								return "Instituto ITN";
-							}
-						})
-						));
+								@Override
+								public String getValue() {
+									return "Instituto ITN";
+								}
+						})));
+	}
+
+
+	private List<SecurityReference> authorization() {
+		AuthorizationScope		scope	= new AuthorizationScope("REST", "Authorized REST Services");
+		AuthorizationScope[]	scopes	= new AuthorizationScope[1];
+		scopes[0] 						= scope;
+		return Collections.singletonList(new SecurityReference("APIKEY", scopes));
 	}
 }
