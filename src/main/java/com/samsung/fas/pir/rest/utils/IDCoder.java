b0781f9a134b7adf777d21840d05caef02130123
Rodrigo Pereira <rodrigo.c.pereira@icloud.com>
2018-04-18 17:33:53 -0400

    [accpetance]  - Removed Base64 import

--- src/main/java/com/samsung/fas/pir/rest/utils/IDCoder.java
+++ src/main/java/com/samsung/fas/pir/rest/utils/IDCoder.java
@@ -9,16 +9,18 @@ import java.util.UUID;
 
 public class IDCoder {
 	public static String encode(UUID id) {
-		UUID		uuid	= Optional.ofNullable(id).orElseThrow(() -> new RESTException("internal.error"));
-		ByteBuffer	buffer	= ByteBuffer.wrap(new byte[16]);
-		buffer.putLong(uuid.getMostSignificantBits());
-		buffer.putLong(uuid.getLeastSignificantBits());
-		return Base64.encodeBase64URLSafeString(buffer.array());
+//		UUID		uuid	= Optional.ofNullable(id).orElseThrow(() -> new RESTException("internal.error"));
+//		ByteBuffer	buffer	= ByteBuffer.wrap(new byte[16]);
+//		buffer.putLong(uuid.getMostSignificantBits());
+//		buffer.putLong(uuid.getLeastSignificantBits());
+//		return Base64.encodeBase64URLSafeString(buffer.array());
+		return id.toString();
 	}
 
 	public static UUID decode(String base) throws RESTException {
-		String		todecode	= Optional.ofNullable(base != null && !base.trim().isEmpty()? base : null).orElse(null);
-		ByteBuffer 	buffer 		= todecode != null? ByteBuffer.wrap(Base64.decodeBase64(todecode)) : null;
-		return buffer != null? new UUID(buffer.getLong(), buffer.getLong()) : null;
+//		String		todecode	= Optional.ofNullable(base != null && !base.trim().isEmpty()? base : null).orElse(null);
+//		ByteBuffer 	buffer 		= todecode != null? ByteBuffer.wrap(Base64.decodeBase64(todecode)) : null;
+//		return buffer != null? new UUID(buffer.getLong(), buffer.getLong()) : null;
+		return UUID.fromString(base);
 	}
 }
